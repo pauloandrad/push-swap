2ec4c87dc79078861064b2623a5e7aea997ee47a
henrygoncalvess <octanebt@gmail.com>
2026-08-08 02:34:54 -0300

    feat: add stack functions and makefile

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -1,9 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hegoncal <hegoncal@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/07 22:43:34 by hegoncal          #+#    #+#             */
+/*   Updated: 2026/08/08 02:26:45 by hegoncal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include "../libs/libft/libft.h"
+# include <limits.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <limits.h>
 
 typedef struct s_node
 {
@@ -50,11 +63,11 @@ typedef struct s_ps
 }	t_ps;
 
 
-typedef void	(*t_fn)(t_push_swap *ps);
+typedef void	(*t_fn)(t_ps *ps);
 
 int		*parse_args(int argc, char **argv, int *count);
 void	check_duplicates(int *values, int count);
-void	ps_error(t_push_swap *ps);
+void	ps_error(t_ps *ps);
 
 t_node	*node_new(int value);
 t_node	*build_stack(int *values, int count);
@@ -63,30 +76,30 @@ void	stack_clear(t_node **top);
 int		stack_size(t_node *top);
 int		is_sorted(t_node *top);
 
-void	op_sa(t_push_swap *ps);
-void	op_sb(t_push_swap *ps);
-void	op_ss(t_push_swap *ps);
-void	op_pa(t_push_swap *ps);
-void	op_pb(t_push_swap *ps);
-void	op_ra(t_push_swap *ps);
-void	op_rb(t_push_swap *ps);
-void	op_rr(t_push_swap *ps);
-void	op_rra(t_push_swap *ps);
-void	op_rrb(t_push_swap *ps);
-void	op_rrr(t_push_swap *ps);
+void	op_sa(t_ps *ps);
+void	op_sb(t_ps *ps);
+void	op_ss(t_ps *ps);
+void	op_pa(t_ps *ps);
+void	op_pb(t_ps *ps);
+void	op_ra(t_ps *ps);
+void	op_rb(t_ps *ps);
+void	op_rr(t_ps *ps);
+void	op_rra(t_ps *ps);
+void	op_rrb(t_ps *ps);
+void	op_rrr(t_ps *ps);
 
 
 double	compute_disorder(t_node *top);
 
 
-void	run_simple(t_push_swap *ps);
-void	run_medium(t_push_swap *ps);
-void	run_complex(t_push_swap *ps);
-void	run_adaptive(t_push_swap);
+void	run_simple(t_ps *ps);
+void	run_medium(t_ps *ps);
+void	run_complex(t_ps *ps);
+void	run_adaptive(t_ps *ps);
 
 
-t_strategy	select_strategy(int argc, char **argv, t_push_swap *ps);
-void		dispatch_strategy(t_push_swap *ps);
+t_strategy	select_strategy(int argc, char **argv, t_ps *ps);
+void		dispatch_strategy(t_ps *ps);
 
 void	print_bench(t_ps *ps);
 
--- src/stack/stack_utils.c
+++ src/stack/stack_utils.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hegoncal <hegoncal@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/07 21:36:19 by hegoncal          #+#    #+#             */
+/*   Updated: 2026/08/08 01:15:35 by hegoncal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /* =============================================================================
  * stack_utils.c
  *
@@ -11,3 +23,68 @@
  * - Funções de debug (ex: print_stack) — úteis durante o desenvolvimento,
  *   NÃO chamar em produção (o subject exige só as operações no stdout).
  * ========================================================================== */
+
+#include "../../includes/push_swap.h"
+
+int	stack_size(t_node *top)
+{
+	int		nodes;
+	t_node	*tmp;
+
+	nodes = 0;
+	tmp = top;
+	while (tmp != NULL)
+	{
+		nodes++;
+		tmp = tmp->next;
+	}
+	return (nodes);
+}
+
+int	is_sorted(t_node *top)
+{
+	if (!top)
+		return (1);
+	while (top->next != NULL)
+	{
+		if (top->value > top->next->value)
+			return (0);
+		top = top->next;
+	}
+	return (1);
+}
+
+t_node	*find_min(t_node *top)
+{
+	t_node	*min;
+	t_node	*tmp;
+
+	if (!top)
+		return (NULL);
+	min = top;
+	tmp = top->next;
+	while (tmp != NULL)
+	{
+		if (tmp->value < min->value)
+			min = tmp;
+		tmp = tmp->next;
+	}
+	return (min);
+}
+
+int	find_min_index(t_node *top)
+{
+	t_node	*min;
+	int		i;
+
+	min = find_min(top);
+	if (!min)
+		return (-1);
+	i = 0;
+	while (top != min)
+	{
+		i++;
+		top = top->next;
+	}
+	return (i);
+}
