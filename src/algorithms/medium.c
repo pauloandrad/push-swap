2e9ec62512ff7c99acce3f6358f8ae0add9bc494
Paulo Henrique De Andrade Junior <pahenriq@c1r1p1.42sp.org.br>
2026-08-23 14:15:35 -0300

    chore: remove print

--- src/algorithms/medium.c
+++ src/algorithms/medium.c
@@ -3,14 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   medium.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hegoncal <hegoncal@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: pahenriq <pahenriq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/15 14:37:43 by pahenriq          #+#    #+#             */
-/*   Updated: 2026/08/22 15:34:36 by hegoncal         ###   ########.fr       */
+/*   Updated: 2026/08/23 14:14:00 by pahenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "print_stack.h"
 #include "push_swap.h"
 #include <stdio.h>
 
@@ -42,31 +41,14 @@ static void	sort_to_b(t_ps *ps)
 	while (ps->a)
 	{
 		index = get_index(ps, ps->a->value);
-		printf("\n--- Avaliando topo de A (Index: %i | Limite: %i) ---\n",
-			index, limit);
 		if (index < limit)
 		{
-			printf("--> [%i < %i] APROVADO! Enviando para B...\n", index,
-				limit);
 			op_pb(ps);
 			if (stack_size(ps->b) == limit)
-			{
 				limit += chunk_size;
-				printf("--> Limite atingido! Próximo limite subiu para %i\n",
-					limit);
-			}
 		}
 		else
-		{
-			printf("--> [%i >= %i] REPROVADO! Rodando A para o final...\n",
-				index, limit);
 			op_ra(ps);
-		}
-		printf("Pilha A: ");
-		print_stack(ps->a);
-		printf("Pilha B: ");
-		print_stack(ps->b);
-		printf("--------------------------------------------------\n");
 	}
 }
 
@@ -79,29 +61,12 @@ static void	return_to_a(t_ps *ps)
 	{
 		index = stack_size(ps->b) - 1;
 		max_pos = get_bmax_position(ps, index);
-		printf("\n--- Procurando o MAIOR (Index: %i) ---\n", index);
 		if (max_pos == 0)
-		{
-			printf("--> ESTÁ NO TOPO! Enviando para A (pa)...\n");
 			op_pa(ps);
-		}
 		else if (max_pos <= stack_size(ps->b) / 2)
-		{
-			printf("--> Posição %i (CIMA). Rodando B pra cima (rb)...\n",
-				max_pos);
 			op_rb(ps);
-		}
 		else
-		{
-			printf("--> Posição %i (BAIXO). Rodando B pra baixo (rrb)...\n",
-				max_pos);
 			op_rrb(ps);
-		}
-		printf("Pilha B: ");
-		print_stack(ps->b);
-		printf("Pilha A: ");
-		print_stack(ps->a);
-		printf("--------------------------------------------------\n");
 	}
 }
 
